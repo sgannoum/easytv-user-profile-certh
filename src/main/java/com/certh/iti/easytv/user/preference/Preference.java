00daed1a103294d581dad21b0429b33366d2bb77
salgan <salgan@iti.gr>
2020-03-13 10:55:57 +0200

    reset font size preferences

--- src/main/java/com/certh/iti/easytv/user/preference/Preference.java
+++ src/main/java/com/certh/iti/easytv/user/preference/Preference.java
@@ -53,7 +53,7 @@ public class Preference implements Clusterable, Comparable<Preference> {
 		put("http://registry.easytv.eu/application/tts/audio/quality", new IntegerAttribute(new double[] {1.0, 8.0}, -1)); 
 	    put("http://registry.easytv.eu/application/cs/cc/subtitles", new SymmetricBinaryAttribute());
 	    put("http://registry.easytv.eu/application/cs/cc/subtitles/language", new LanguageAttribute());
-	    put("http://registry.easytv.eu/application/cs/cc/subtitles/font/size", new IntegerAttribute(new double[] {0.0, 100.0}, 1.0, 25, -1));
+	    put("http://registry.easytv.eu/application/cs/cc/subtitles/font/Size", new IntegerAttribute(new double[] {0.0, 100.0}, 1.0, 25, -1));
 	    put("http://registry.easytv.eu/application/cs/cc/subtitles/font/color", new ColorAttribute());
 	    put("http://registry.easytv.eu/application/cs/cc/subtitles/background/color", new ColorAttribute());
 	    put("http://registry.easytv.eu/application/cs/cc/audio/subtitle",  new SymmetricBinaryAttribute());
